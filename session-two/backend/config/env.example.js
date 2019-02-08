b71293ee592fce0505f20ed22a25a13388031dda
Aaron Welsh <aaronwelsh505@gmail.com>
2019-02-08 15:11:47 +0000

    Example of a env.js file.

---
+++ session-two/backend/config/env.example.js
@@ -0,0 +1,7 @@
+/**
+ * @description Define your MongoDB database URL here. Do not share this file with anyone as it may contain sensitive information.
+ * @note Change this file to env.js
+ */
+module.exports = {
+    mongo_url: 'mongodb://localhost:27017/blog' // Your URL here. For example, a localhost one.
+};
